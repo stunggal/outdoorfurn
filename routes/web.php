973918ed5fa07f821ef2a7ab5d88296b7c2af464
wahid alfaridsi <walfaridsi@gmail.com>
2023-06-04 08:26:28 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// to dashboard controller
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
